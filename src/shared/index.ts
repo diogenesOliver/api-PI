f87844cdafd571afdeb0c8562c2e8ddea86ec2b0
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2023-02-16 17:46:43 +0000

    feat: Provider by sendEmail created

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -3,7 +3,15 @@ import { container } from 'tsyringe'
 import { ISaveUserInformationsRepository } from "../repositories/IUserInformationRepository";
 import { SaveUserInformationsRepository } from "../repositories/implementations/UserInformationRepository";
 
+import { IMailProvider } from '../providers/IMailProvider';
+import { MailProvider } from '../providers/implementation/MailProvider';
+
 container.registerSingleton<ISaveUserInformationsRepository>(
     "SaveUserInformationsRepository",
     SaveUserInformationsRepository
+)
+
+container.registerSingleton<IMailProvider>(
+    "MailProvider",
+    MailProvider
 )
--- src/useCases/UserInformationsService.ts
+++ src/useCases/UserInformationsService.ts
@@ -2,18 +2,34 @@ import { inject, injectable } from "tsyringe";
 
 import { ISaveUserInformationsRepository } from "../repositories/IUserInformationRepository";
 import { UserInformationEntitie } from "../entities/UserInformationEntitie";
+import { IMailProvider } from "../providers/IMailProvider";
 
 @injectable()
-export class UserInformationsService{
+export class UserInformationsService {
 
     constructor(
         @inject("SaveUserInformationsRepository")
-        private informationsService: ISaveUserInformationsRepository
-    ){}
+        private informationsService: ISaveUserInformationsRepository,
+        @inject("MailProvider")
+        private mailProviderService: IMailProvider
+    ) { }
 
-    execute(data: UserInformationEntitie){
+    execute(data: UserInformationEntitie) {
         this.informationsService.save(data)
         return data
+
+        this.mailProviderService.sendMail({
+            to: {
+                name: data.props.nome,
+                email: data.props.email
+            },
+            from: {
+                name: 'Equipe do meu app',
+                email: 'diogenes.riboliveira@gmail.com'
+            },
+            subject: 'Seja bem-vindo ao SenacFIT',
+            body: '<p> Seja bem-vindo a melhor plataforma FIT </p>'
+        })
     }
 
 }
