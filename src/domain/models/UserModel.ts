410139c3bb1cf07b35ecd6e55bb0efee4bc0c817
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2023-02-28 19:00:20 +0000

    feat: UserSchema Created

---
+++ src/domain/models/UserModel.ts
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+
+interface IUser {
+    nome: string,
+    email: string,
+    confirmEmail: string,
+    cpf: string
+}
+
+const userSchema = new Schema<IUser>({
+    nome: { type: String, require: true },
+    email: { type: String, require: true },
+    confirmEmail: { type: String, require: true },
+    cpf: { type: String, require: true }
+})
+
+const User = model<IUser>('User', userSchema)
+
+export { User }
