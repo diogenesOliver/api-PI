e310890c5f64fc6ec1e33b061b08486fa4a8ee12
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2023-03-30 19:01:43 +0000

    IP: Spliting header

--- src/domain/middleware/JTWMiddleware/JwtHandleMiddleware.ts
+++ src/domain/middleware/JTWMiddleware/JwtHandleMiddleware.ts
@@ -1,15 +1,22 @@
-import { Request, Response, NextFunction } from "express";
+import { Response, NextFunction } from "express";
 import { verify } from 'jsonwebtoken'
 
 class JWTHandleValidation {
 
-    verifyJWT(req: Request, res: Response, next: NextFunction) {
+    verifyJWT(req: any, res: Response, next: NextFunction) {
         const SECRET_KEY = process.env.SECRET_TOKEN as string
         const authHeader = req.headers['x-access-token'] as string
+        const token = authHeader && authHeader.split(" ")[0]
 
-        const token = verify(authHeader, SECRET_KEY, (err, decode) => {
-            if(err)
-                res.status(401).json({ msg: 'ERROR[401] - Authenticate error' })
+        if (!token)
+            return res.status(401).json({ msg: "ERROR[401] - unauthorized" })
+
+        verify(token, SECRET_KEY, (err: any, user: any) => {
+            if (err)
+                return res.status(403).json({ msg: "Forbiden" })
+
+            req.user = user
+            next()
         })
 
     }
