b1e7b2a12ccc1ad0ececbfb3617714fafd866179
Diógenes Ribeiro de Oliveira <91908375+diogenesOliver@users.noreply.github.com>
2023-05-18 18:39:53 +0000

    feat: Creating generic file to export interaface of the repositories

--- src/repositories/implementations/UserInformationRepository.ts
+++ src/repositories/implementations/UserInformationRepository.ts
@@ -3,9 +3,11 @@ import { UserInformationEntitie } from "../../entities/UserInformationEntitie";
 
 import { User } from "../../domain/models/UserModel";
 
-export class SaveUserInformationsRepository implements ISaveUserInformationsRepository {
+import { ISaveFunctionGeneric } from '../GenericRepository'
+
+/* export class SaveUserInformationsRepository implements ISaveUserInformationsRepository {
     save(datas: UserInformationEntitie): UserInformationEntitie {
         User.create(datas)
         return datas
     }
-}
+} */
