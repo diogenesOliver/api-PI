0d7ef07ae2c339ecf96df7afd473ce5b049aad7e
Diógenes Ribeiro de Oliveira <91908375+diogenesOliver@users.noreply.github.com>
2023-06-06 20:03:01 +0000

    feat: Checking if email and password  checking

--- src/CreateUser/CreateUserController.ts
+++ src/CreateUser/CreateUserController.ts
@@ -2,17 +2,23 @@ import { Request, Response } from "express";
 import { CreateUserService } from "./CreateUserService";
 import { UserData } from "@prisma/client";
 
-export class CreateUserController{
+export class CreateUserController {
     constructor(
         private createUserService: CreateUserService
-    ){}
-    
+    ) { }
+
     async createUserController(req: Request, res: Response) {
         const userData: UserData = req.body
         this.createUserService.save(userData)
 
-        res.status(200).send('Succesfuly to creta User')
-
-        return userData
+        if (userData.email != userData.confirmar_email){
+            res.status(404).send('ERROR in password or Email')
+        }else if(userData.senha != userData.confirmar_senha){
+            res.status(404).send('ERROR in password or Email')
+        }else{
+            res.status(200).send('Succesfuly to creta User')
+        }
+            return userData
+        
     }
 }
