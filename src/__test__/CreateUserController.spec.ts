de976cc7febfd584e8d220b36d014591277317b5
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-06-08 21:58:21 -0300

    feat: Integration test to create user created

---
+++ src/__test__/CreateUserController.spec.ts
@@ -0,0 +1,27 @@
+import 'reflect-metadata'
+import request from 'supertest'
+
+import { app } from '../main'
+
+const userDataFromtest = {
+    email: "oliveira@example.com",
+    confirmar_email: "oliveira@example.com",
+    senha: "senha123",
+    confirmar_senha: "senha123",
+    nome: "Diógenes",
+    idade: 25,
+    peso: 70.5,
+    altura: 1.75,
+    res_fisica: "Lesão no Joelho",
+    res_alimentar: "Lactose"
+}
+
+describe('Integration test to create one User by route /login', () => {
+    it('Should be return status code 200', async () => {
+        const response = await request(app)
+            .post('/login')
+            .send(userDataFromtest)
+
+        expect(response.status).toEqual(200)
+    })
+})
