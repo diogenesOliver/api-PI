9b52c9596d5d760935ae33740275efc2a252b57c
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-06-15 15:28:01 -0300

    feat: new form and v-mask dep

--- web/src/main.js
+++ web/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueMask from 'v-mask'
 
+Vue.use(VueMask);
 Vue.config.productionTip = false
 
 new Vue({
--- web/src/views/UserForm.vue
+++ web/src/views/UserForm.vue
@@ -4,70 +4,147 @@
     <section>
       <div class="title">
         <h2>Bem-vindo, Fulano!</h2>
-        <p>Por gentileza, complete seu cadastro com alguns dados pessoais e assim prosseguirmos para seus treinamentos.</p>
-        <form class="contact-inform form">
-          <div class="form-control">
-            <input
-              type="text"
-              name="name"
-              class="form-input"
-              placeholder="none"
-              required
-            />
-            <label for="name" class="form-label">Nome</label>
-          </div>
-          <div class="form-control">
-            <input
-              type="password"
-              name="password"
-              class="form-input"
-              placeholder="none"
-              required
-            />
-            <label for="password" class="form-label">Senha</label>
-          </div>
-        </form>
+        <p>
+          Por gentileza, complete seu cadastro com alguns dados pessoais para
+          conhecermos um pouco mais sobre você.
+        </p>
       </div>
+      <form class="contact-inform form">
+        <div class="input-columns">
+          <div class="form-control">
+            <input
+              v-model="user.height"
+              v-mask="'#.##m'"
+              type="email"
+              name="email"
+              class="form-input"
+              placeholder="none"
+              required
+            />
+            <label for="email" class="form-label">Altura</label>
+          </div>
+          <div class="form-control">
+            <input
+              v-model="user.weight"
+              v-mask="'##.#kg'"
+              type="email"
+              name="email"
+              class="form-input"
+              placeholder="none"
+              required
+            />
+            <label for="email" class="form-label">Peso</label>
+          </div>
+        </div>
+        <div class="input-columns">
+          <div class="form-control">
+            <input
+              v-model="user.age"
+              v-mask="'##'"
+              type="email"
+              name="email"
+              class="form-input"
+              placeholder="none"
+              required
+            />
+            <label for="email" class="form-label">Idade</label>
+          </div>
+          <div class="form-control">
+            <input
+              v-model="user.idk"
+              v-mask="'##.#kg'"
+              type="email"
+              name="email"
+              class="form-input"
+              placeholder="none"
+              required
+            />
+            <label for="email" class="form-label">Peso</label>
+          </div>
+        </div>
+        <div class="food-group">
+          <div class="title">
+            <h3>Qual o seu grupo alimentar?</h3>
+            <div class="input-box">
+              <input
+                type="radio"
+                name="food-radio"
+                id="carnivore"
+                v-model="isCarnivore"
+              />
+              <label for="carnivore">Carnivoro</label>
+            </div>
+            <div class="input-box">
+              <input
+                type="radio"
+                name="food-radio"
+                id="vegetarian"
+                v-model="isVeg"
+              />
+              <label for="vegetarian">Vegetariano</label>
+            </div>
+            <div class="input-box">
+              <input
+                type="radio"
+                name="food-radio"
+                id="vegan"
+                v-model="isVegan"
+              />
+              <label for="vegan">Vegano</label>
+            </div>
+          </div>
+        </div>
+      </form>
     </section>
   </main>
 </template>
 
 <script>
-import UserHeader from '@/components/UserHeader.vue';
+import UserHeader from "@/components/UserHeader.vue";
 export default {
   name: "UserForm",
 
+  data() {
+    return {
+      user: {
+        height: "",
+        weight: "",
+      },
+    };
+  },
+
   components: {
-    UserHeader
-  }
-}
+    UserHeader,
+  },
+};
 </script>
 
 <style scoped>
 main {
-  height: 100vh;
+  height: 85vh;
+}
+
+section {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
   background: #fff;
 }
 
 .title {
-
-  margin: 0 auto;
   color: black;
+  background: #ffffff;
 }
 
-section {
-  height: 85%;
-  width: 80%;
-  margin: 0 auto;
-  background: dodgerblue;
-}
 .contact-inform {
-  width: 50%;
-  min-height: 47%;
+  width: 45%;
+  min-height: 50%;
   padding: 4rem 2rem 2rem;
   border-radius: 0.25rem;
   color: black;
-  background: #ffffff;
 }
 
 .input-columns {
@@ -146,4 +223,14 @@ section {
   font-weight: 500;
   transition: all 0.2s ease-in-out;
 }
+
+.input-box {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+
+/* .input-box input {
+  display: flex;
+} */
 </style>
