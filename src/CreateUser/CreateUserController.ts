6fc2eb2d5a3523a8493fdb1d4889d029b0dd1bfe
diogenesOliver <diogenes.riboliveira@gmail.com>
2023-06-29 14:45:33 -0300

    fix: install cors dependencie from fix CORS Error

--- src/CreateUser/CreateUserController.ts
+++ src/CreateUser/CreateUserController.ts
@@ -12,14 +12,14 @@ export class CreateUserController {
     async createUserController(req: Request, res: Response) {
         const userData: UserData = req.body
 
-        const salt = await bcrypt.genSalt(14)
-        const bcryptPassword = await bcrypt.hash(userData.senha, salt)
+        /* const salt = await bcrypt.genSalt(14)
+        const bcryptPassword = await bcrypt.hash(userData.senha, salt) */
 
         if (userData.email != userData.confirmar_email || userData.senha != userData.confirmar_senha)
             res.status(404).send('ERROR in password or Email')
 
-        userData.senha = bcryptPassword
-        userData.confirmar_senha = bcryptPassword
+        /* userData.senha = bcryptPassword
+        userData.confirmar_senha = bcryptPassword */
 
         await this.createUserService.save(userData)
 
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import 'reflect-metadata'
 
 require('dotenv').config()
+const cors = require("cors")
 
 import express from 'express'
 import { routes } from './routes'
+import path from 'path'
 
 const app = express()
 
-app.use(express.json())
+app.set('view engine', 'vue')
+app.set('views', path.join(__dirname, '../web'))
+
+app.use(cors())
+app.use(express.json(), express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, '../web')))
 app.use(routes)
 
 export { app }
